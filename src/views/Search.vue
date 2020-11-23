d0c88de34d3e770009b03da90d089ad5e099a26c
THH_MAC <18210240177@fudan.edu.cn>
2020-11-23 20:58:05 +0800

    更新首页内容

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="search">
-        <p class="title">跨库搜索</p>
+        <p class="title">Cross Database Retrieval</p>
         <div class="serach-com-wrapper">
             <el-input v-model="input" placeholder="请输入内容" style="width: 60%"></el-input>
             <div class="button-group">
-                 <el-button type="success" @click="$router.push('/search-result?n1=1&n3=6')">关键词搜索</el-button>
-                 <el-button type="warning">SQL语句搜索</el-button>
+                 <el-button type="primary" @click="$router.push('/search-result?n1=1&n3=6')">search by keywords</el-button>
+                 <!-- <el-button type="warning">SQL语句搜索</el-button> -->
             </div>
         </div>
 
         <div class="database-container">
-                <DatasetButton :src="require('@/assets/临床.png')" title="临床数据库"/>
-                <DatasetButton :src="require('@/assets/医学影像.png')" title="医学影像库"/>
-                <DatasetButton :src="require('@/assets/组学.png')" title="组学数据库"/>
-                <DatasetButton :src="require('@/assets/text.png')" title="文档库"/>
+                <DatasetButton :src="require('@/assets/医学影像.png')" title="Medical Image" desc="The novel coronavirus pneumonia medical imaging data, novel coronavirus pneumonia CT image and other CT images of pneumonia or normal patients."/>
+                <DatasetButton :src="require('@/assets/组学.png')" title="Omics" desc="The novel coronavirus pneumonia data include 2019 novel coronavirus, SARS and MERS nucleotide sequence." />
+                <DatasetButton :src="require('@/assets/临床.png')" title="Clinical Medicil" desc="The novel coronavirus pneumonia clinical data include novel coronavirus pneumonia patients' clinical data management."/>
+                <DatasetButton :src="require('@/assets/text.png')" title="BioMedical Knowledge" desc="The novel coronavirus pneumonia related biomedical knowledge novel coronavirus pneumonia related subjects are English and biomedical knowledge."/>
             </div>
     </div>
 </template>
@@ -36,24 +36,27 @@ export default {
 <style lang="less" scoped>
 .search {
     .title {
-    font-size: 25px;
+    font-size: 40px;
     font-weight: 600;
+    color: #fff;
+    margin: 15px 0;
   }
   .serach-com-wrapper{
       margin: 0 auto;
-      width: 60%;
+      width: 90%;
       display: flex;
-      justify-content: space-around;
+      justify-content: center;
   }
   .el-button-group{
-      width: 20%;
+      width: 30%;
       display: flex;
       justify-content: space-around;
 
   }
   .database-container{
-      margin: 90px auto;
+      margin: 40px auto;
       display: flex;
+      flex-wrap: wrap;
       width: 70%;
       justify-content: space-around;
   }
